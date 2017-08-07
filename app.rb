6a4f5284ce6bfb420f15df61756911b1415aee10
Stacey Astoveza <staceyastoveza@transfix.io>
2017-08-07 14:02:44 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	get '/name' do 
+		"My name is Stacey."
+	end
+
+	get '/hometown' do
+		"My hometown is Hayward, CA."
+	end
+
+	get '/favorite-song' do
+		"My favorite song is Nothing's gonna stop us now."
+	end
 end
